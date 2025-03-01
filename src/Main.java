82a5e4aca3a0cd6f613a24d8c99697e7fb355207
hemanth_mahankali <hemanth_mahankali.hemanth2126@gmail.com>
2025-02-28 22:06:41 -0500

    project setup

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public static void addition(String[] args){
+        String name = "hemanth";
+        int a = 1;
+        double b = 1.5;
+        double c = a + b;
+        System.out.println(a);
+    }
 }
